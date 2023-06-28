b1d4c13ed80d0b2fa7d72a8781e34b7fa5ff96a7
Jeremy Yin <jeremyyin2012@gmail.com>
2023-06-28 17:13:12 +0800

    注意仔细审题：a-Z A-z “两者它们之间的字符”的意思，ascii码是一个指定序列，是从这个序列里面取区间。不是说大小写打印顺序问题。

--- src/az/az2.rs
+++ src/az/az2.rs
@@ -1,13 +1,8 @@
 
 // print A-z
 pub fn az2() {
-    let az_lower = 'a' ..= 'z';
-    let az_upper = 'A' ..= 'Z';
-    let az = [az_upper, az_lower];
-    for r in az {
-        for c in r {
-            print!("{c}");
-        }
+    for c in 'A' ..= 'z' {
+        print!("{c}");
     }
     println!("\nDone");
 }
