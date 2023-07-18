a0ad86c126586337fed551878243d3c59f2eedc7
Jeremy Yin <jeremyyin2012@gmail.com>
2023-07-18 22:03:05 +0800

    接入数据库后格式化建表语句

--- lesson03/src/sql/table.sql
+++ lesson03/src/sql/table.sql
@@ -1,26 +1,26 @@
 -- 班级
 CREATE TABLE "public"."class"
 (
-    "id" uuid NOT NULL,
+    "id"        uuid      NOT NULL,
     "code"      text      NOT NULL,
     "name"      text      NOT NULL,
     "kind"      numeric   NOT NULL,
     "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
+    "create_by" uuid      NOT NULL,
     "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "update_by" uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
 -- 班级-学员
 CREATE TABLE "public"."class_student"
 (
-    "class_id" uuid NOT NULL,
-    "student_id" uuid NOT NULL,
-    "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
-    "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "class_id"   uuid      NOT NULL,
+    "student_id" uuid      NOT NULL,
+    "create_dt"  timestamp NOT NULL,
+    "create_by"  uuid      NOT NULL,
+    "update_dt"  timestamp NOT NULL,
+    "update_by"  uuid      NOT NULL,
     PRIMARY KEY ("class_id", "student_id")
 );
 
@@ -28,14 +28,14 @@ CREATE TABLE "public"."class_student"
 -- 学员
 CREATE TABLE "public"."student"
 (
-    "id" uuid NOT NULL,
+    "id"        uuid      NOT NULL,
     "code"      text      NOT NULL,
     "name"      text      NOT NULL,
     "kind"      numeric   NOT NULL,
     "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
+    "create_by" uuid      NOT NULL,
     "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "update_by" uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -43,14 +43,14 @@ CREATE TABLE "public"."student"
 -- 成绩
 CREATE TABLE "public"."grades"
 (
-    "id" uuid NOT NULL,
-    "student_id" uuid NOT NULL,
-    "course_id" uuid NOT NULL,
-    "score"     numeric   NOT NULL,
-    "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
-    "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "id"         uuid      NOT NULL,
+    "student_id" uuid      NOT NULL,
+    "course_id"  uuid      NOT NULL,
+    "score"      numeric   NOT NULL,
+    "create_dt"  timestamp NOT NULL,
+    "create_by"  uuid      NOT NULL,
+    "update_dt"  timestamp NOT NULL,
+    "update_by"  uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -58,14 +58,14 @@ CREATE TABLE "public"."grades"
 -- 教师
 CREATE TABLE "public"."teacher"
 (
-    "id" uuid NOT NULL,
+    "id"        uuid      NOT NULL,
     "code"      text      NOT NULL,
     "name"      text      NOT NULL,
     "kind"      numeric   NOT NULL,
     "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
+    "create_by" uuid      NOT NULL,
     "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "update_by" uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -73,14 +73,14 @@ CREATE TABLE "public"."teacher"
 -- 课程
 CREATE TABLE "public"."course"
 (
-    "id" uuid NOT NULL,
+    "id"        uuid      NOT NULL,
     "code"      text      NOT NULL,
     "name"      text      NOT NULL,
     "kind"      numeric   NOT NULL,
     "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
+    "create_by" uuid      NOT NULL,
     "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "update_by" uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -88,16 +88,16 @@ CREATE TABLE "public"."course"
 -- 任课
 CREATE TABLE "public"."teach"
 (
-    "id" uuid NOT NULL,
-    "kind"      numeric   NOT NULL,
-    "year"      numeric   NOT NULL,
-    "semester"  numeric   NOT NULL,
-    "course_id" uuid NOT NULL,
-    "teacher_id" uuid NOT NULL,
-    "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
-    "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "id"         uuid      NOT NULL,
+    "kind"       numeric   NOT NULL,
+    "year"       numeric   NOT NULL,
+    "semester"   numeric   NOT NULL,
+    "course_id"  uuid      NOT NULL,
+    "teacher_id" uuid      NOT NULL,
+    "create_dt"  timestamp NOT NULL,
+    "create_by"  uuid      NOT NULL,
+    "update_dt"  timestamp NOT NULL,
+    "update_by"  uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -105,13 +105,13 @@ CREATE TABLE "public"."teach"
 -- 配课
 CREATE TABLE "public"."selection"
 (
-    "id" uuid NOT NULL,
-    "class_id" uuid NOT NULL,
-    "teach_id" uuid NOT NULL,
+    "id"        uuid      NOT NULL,
+    "class_id"  uuid      NOT NULL,
+    "teach_id"  uuid      NOT NULL,
     "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
+    "create_by" uuid      NOT NULL,
     "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "update_by" uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -119,15 +119,15 @@ CREATE TABLE "public"."selection"
 -- 上课
 CREATE TABLE "public"."schedule"
 (
-    "id" uuid NOT NULL,
-    "selection_id" uuid NOT NULL,
-    "start_dt"  timestamp NOT NULL,
-    "end_dt"    timestamp NOT NULL,
-    "classroom_id" uuid NOT NULL,
-    "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
-    "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "id"           uuid      NOT NULL,
+    "selection_id" uuid      NOT NULL,
+    "start_dt"     timestamp NOT NULL,
+    "end_dt"       timestamp NOT NULL,
+    "classroom_id" uuid      NOT NULL,
+    "create_dt"    timestamp NOT NULL,
+    "create_by"    uuid      NOT NULL,
+    "update_dt"    timestamp NOT NULL,
+    "update_by"    uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
 
@@ -135,15 +135,15 @@ CREATE TABLE "public"."schedule"
 -- 考试
 CREATE TABLE "public"."exam"
 (
-    "id" uuid NOT NULL,
-    "code"      text      NOT NULL,
-    "name"      text      NOT NULL,
-    "kind"      numeric   NOT NULL,
-    "selection_id" uuid NOT NULL,
-    "score"     numeric   NOT NULL,
-    "create_dt" timestamp NOT NULL,
-    "create_by" uuid NOT NULL,
-    "update_dt" timestamp NOT NULL,
-    "update_by" uuid NOT NULL,
+    "id"           uuid      NOT NULL,
+    "code"         text      NOT NULL,
+    "name"         text      NOT NULL,
+    "kind"         numeric   NOT NULL,
+    "selection_id" uuid      NOT NULL,
+    "score"        numeric   NOT NULL,
+    "create_dt"    timestamp NOT NULL,
+    "create_by"    uuid      NOT NULL,
+    "update_dt"    timestamp NOT NULL,
+    "update_by"    uuid      NOT NULL,
     PRIMARY KEY ("id")
 );
