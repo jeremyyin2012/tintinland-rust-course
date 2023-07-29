e122aac1634434bb7f9de366ac423efa315d742e
Jeremy Yin <jeremyyin2012@gmail.com>
2023-07-29 23:50:14 +0800

    sum if xlookup 尝试用声明宏试试基本函数实现，其实并不是很好

--- lesson05/src/main.rs
+++ lesson05/src/main.rs
@@ -1,3 +1,48 @@
+/// =SUM(number1,[number2],...)
+macro_rules! sum {
+    ($($x:expr),*) => {
+        {
+            0$(+$x)*
+        }
+    };
+}
+
+/// =IF(C2=1,”Yes”,”No”)
+macro_rules! r#if {
+    ($c:expr, $x:expr, $y: expr) => {
+        {
+            if $c {
+                $x
+            } else {
+                $y
+            }
+        }
+    };
+}
+
+/// =XLOOKUP(lookup_value, lookup_array, return_array, [if_not_found], [match_mode], [search_mode])
+macro_rules! xlookup {
+    ($v:expr,$la:expr,$ra:expr,$d:expr,$mm:expr,$sm:expr) => {
+        {
+            // todo mm sm
+            let mut res = $d;
+            for (idx, item) in $la.iter().enumerate() {
+                if $v == *item {
+                    res = $ra[idx];
+                    break;
+                }
+            };
+            res
+        }
+    };
+}
+
 fn main() {
     println!("Hello, world!");
+    let s = sum!(1,2,3,4,3+4,100-98,2/1,4*8,9/3,3/2);
+    println!("{s}");
+    let r = r#if!(3>0, 1, 0);
+    println!("{r}");
+    let res = xlookup!(2, vec![1,2,3], vec!["a","b","c"], "a", 1, 1);
+    println!("{res:?}");
 }
